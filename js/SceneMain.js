6755e53dc960b5f52014278dfe68d956ca049cc0
Elbie-em <emoonga1993@gmail.com>
2020-09-03 22:12:58 +0200

    Preload assets and Fix indentation

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -1,17 +1,24 @@
 class SceneMain extends Phaser.Scene {
-    constructor() {
+	constructor() {
 		super({ key: "SceneMain" });
-    }
-    
-    preload() {
+	}
 
-    }
+	preload() {
+		this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+	}
 
-    create() {
+	create() {
 
-    }
+	}
 
-    update() {
-        
-    }
+	update() {
+
+	}
 }
--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    autoCenter: true,
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	autoCenter: true,
 	backgroundColor: "#4488AA",
-    scene: [SceneMain]
+	scene: [SceneMain]
 };
 
 const game = new Phaser.Game(config);
