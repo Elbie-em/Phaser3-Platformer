099d4ca01756ad34b0825c430a7c192c6adc1a7f
Elbie-em <emoonga1993@gmail.com>
2020-09-03 22:19:14 +0200

    Rename assets to res, add background to game

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -4,18 +4,18 @@ class SceneMain extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
+		this.load.image('sky', 'res/sky.png');
+    this.load.image('ground', 'res/platform.png');
+    this.load.image('star', 'res/star.png');
+    this.load.image('bomb', 'res/bomb.png');
     this.load.spritesheet('dude', 
-        'assets/dude.png',
+        'res/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
 	}
 
 	create() {
-
+		this.add.image(400, 300, 'sky');
 	}
 
 	update() {
