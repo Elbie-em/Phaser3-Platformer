2f803e31f382570e0b57ac1eb2710c952eb495ad
Elbie-em <emoonga1993@gmail.com>
2020-09-03 23:17:39 +0200

    Add static bodies to game

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -15,7 +15,17 @@ class SceneMain extends Phaser.Scene {
 	}
 
 	create() {
+		// Add the sky
 		this.add.image(400, 300, 'sky');
+		
+		//Add the static bodies i.e the ground
+		const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();// double size
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 	}
 
 	update() {
