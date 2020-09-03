32e7d6d8224fbfc2b53950e89fdff50f58c1c218
Elbie-em <emoonga1993@gmail.com>
2020-09-03 21:55:40 +0200

    Configure Phase game

---
+++ js/game.js
@@ -0,0 +1,10 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    autoCenter: true,
+	backgroundColor: "#4488AA",
+    scene: [SceneMain]
+};
+
+const game = new Phaser.Game(config);
